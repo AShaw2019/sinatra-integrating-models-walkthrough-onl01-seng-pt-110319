61139690fdd0aaad752f92ad88f1b6d884d17a4f
Ashley Twomey <aprier091012@gmail.com>
2020-03-01 16:21:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,9 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
+ post '/' do
   text_from_user = params[:user_text]
- 
   @analyzed_text = TextAnalyzer.new(text_from_user)
- 
   erb :results
 end
 end
